d88360878a0f7a1746db345be40b5600f54ff517
devfeb02 <emaildp007@gmail.com>
2019-09-17 13:30:38 +0530

    my java commit

---
+++ myclass1.java
@@ -0,0 +1,17 @@
+
+public class myclass1 {
+	//Fibonacci Series using Recursion  
+	    static int fib(int n) 
+	    { 
+	    if (n <= 1) 
+	       return n; 
+	    return fib(n-1) + fib(n-2); 
+	    } 
+	       
+	    public static void main (String args[]) 
+	    { 
+	    int n = 9; 
+	    System.out.println(fib(n)); 
+	    } 
+	} 
+	/* This code is contributed by deba */
